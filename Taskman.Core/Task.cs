c5439e1bbef2146efc99dede8f919af19af1ecce
ForumPlayer <fp+git@forumplayer.dev>
2026-05-24 05:01:16 -0300

    Create a class for the Task itself

---
+++ Taskman.Core/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Taskman.Core;
+
+public class TaskNode {
+
+    public TaskNode(string name) {
+        this.Name = name;
+    }
+
+
+    public string Name { get; private set; } = string.Empty;
+    public string? Description;
+
+
+    public DateTime? Deadline { get; private set; }
+    public TaskStatus Status { get; private set; } = TaskStatus.Pending;
+
+}
+
+public enum TaskStatus { Pending, Done }
