57471946cbc8e3f48ce3fc477eee7f878c467541
ForumPlayer <fp+git@forumplayer.dev>
2026-05-24 05:16:09 -0300

    Split the TaskNode class + Add task dependency

--- Taskman.Core/Task.cs
+++ Taskman.Core/Task.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 
 namespace Taskman.Core;
 
-public class TaskNode {
+public class TaskNode : TaskProperties {
 
     public TaskNode(string name) {
         this.Name = name;
     }
 
+    public List<TaskNode> RequiredBy = [];
+    public List<TaskNode> Requires = [];
 
-    public string Name { get; private set; } = string.Empty;
+}
+
+public class TaskProperties {
+    public string Name { get; protected set; } = string.Empty;
     public string? Description;
 
-
-    public DateTime? Deadline { get; private set; }
-    public TaskStatus Status { get; private set; } = TaskStatus.Pending;
-
+    public DateTime? Deadline { get; protected set; }
+    public TaskStatus Status { get; protected set; } = TaskStatus.Pending;
 }
 
 public enum TaskStatus { Pending, Done }
